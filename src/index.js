887bb5af860a003bbcdf69226d37f5b2e4a603ff
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2025-03-15 22:04:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// Load environment variables
 dotenv.config();
 
-app.listen(3000, () => console.log('Server running......'));
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
+// Start the server
+app.listen(3000, () => console.log('Server running on port 3000...'));
